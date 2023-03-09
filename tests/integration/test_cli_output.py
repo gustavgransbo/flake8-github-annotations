1e861dd9e3130bc44d827b4e9b7c90e98f3f70e4
Gustav Gränsbo <gustav.gransbo@gmail.com>
2023-03-09 21:59:45 +0100

    Add integration test of cli output
    This test fails, due to a pre exisitng bug in the formatter!

---
+++ tests/integration/test_cli_output.py
@@ -0,0 +1,30 @@
+import subprocess
+import sys
+import tempfile
+
+import pytest
+
+
+@pytest.fixture
+def file_with_error() -> str:
+    """Yields a path to a temprary file that contains a F401 error"""
+    with tempfile.NamedTemporaryFile("w+") as f:
+        f.write("import foo\n")
+        f.flush()
+        yield f.name
+
+
+def test_output_is_formated_as_github_error_annotation(file_with_error: str):
+    output = subprocess.run(
+        [
+            sys.executable,  # This makes sure flake8 is ran from the correct venv
+            "-m",
+            "flake8",
+            file_with_error,
+            "--format",
+            "github-annotations",
+        ],
+        capture_output=True,
+    )
+    expected_output = f"::error file={file_with_error},line=1,col=1,title=F401::'foo' imported but unused\n"  # noqa:E501
+    assert output.stdout.decode() == expected_output
